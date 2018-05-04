ab63390fda79c50bd00085677ee6ae5edc3318e6
Nil Mestre Mas <nil.mestre@gmail.com>
2018-05-04 11:19:50 +0200

    Diapositiva 14

---
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -0,0 +1,9 @@
+package com.drpicox.bowlingKata;
+
+import junit.framework.TestCase;
+
+public class BowlingGameTest extends TestCase {
+    public void testGutterGame() throws Exception {
+        Game g = new Game();
+    }
+}
