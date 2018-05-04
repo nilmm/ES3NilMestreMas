76017b033d0a141db6cc80e963b53419d4e7845e
Nil Mestre Mas <nil.mestre@gmail.com>
2018-05-04 11:26:34 +0200

    Diapositiva 28

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -6,13 +6,13 @@ public class BowlingGameTest extends TestCase {
     protected void setUp() throws Exception {
         g = new Game();
     }
-    public void testGutterGame() throws Exception {
-        rollMany(20, 0);
-        assertEquals(0, g.score());
-    }
     private void rollMany(int n, int pins) {
         for (int i = 0; i < n; i++)
             g.roll(pins);
+    }
+    public void testGutterGame() throws Exception {
+        rollMany(20, 0);
+        assertEquals(0, g.score());
     }
     public void testAllOnes() throws Exception {
         rollMany(20,1);
